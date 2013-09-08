c6dde2493d83db258b3432c181ca09e28e8424ce
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2013-09-08 21:27:44 +0100

    run gofmt

--- nw.go
+++ nw.go
@@ -2,82 +2,68 @@
 package main
 
 import (
-    "fmt"
-    "os"
-//    "io"
-    "strconv"
-//    "log"
-    "bufio"
-    "github.com/atotto/clipboard"
-    "bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"bufio"
+	"bytes"
+	"github.com/atotto/clipboard"
 )
 
 func longestFile(line []rune) int {
-    max := 0
-    for i, _ := range line {
-        slice := line[0:i]
-        file := string(slice)
-        if _, err := os.Stat(file); err == nil {
-//          log.Println("file exists")
-//          log.Println(file)
-          max = i
-        }
-    }
-    return max
+	max := 0
+	for i, _ := range line {
+		slice := line[0:i]
+		file := string(slice)
+		if _, err := os.Stat(file); err == nil {
+			max = i
+		}
+	}
+	return max
 }
 
 func main() {
-    var clip bytes.Buffer
-    argsWithoutProg := os.Args[1:]
-    //fmt.Println(argsWithoutProg)
-    reader := bufio.NewReader(os.Stdin)
+	var clip bytes.Buffer
+	argsWithoutProg := os.Args[1:]
 
-    i := 0
+	reader := bufio.NewReader(os.Stdin)
 
-    for {
-        line, err := reader.ReadString('\n')
+	i := 0
 
-        if err != nil {
-            // You may check here if err == io.EOF
-            //if err == io.EOF {
-            //}
-            break
-        }
+	for {
+		line, err := reader.ReadString('\n')
 
-        //file := make([]rune, len(line))
-        longest := 0
-        start := 0
-        for i, _ := range line {
-            search := []rune(line[i:len(line)])
-            found := longestFile(search)
-            pos := found + i + 1
-            if found > 0 && pos > longest {
-                longest = pos 
-                start = i
-            }
-        }
+		if err != nil {
+			// check here if err == io.EOF
+			break
+		}
 
-        i = i + 1
+		longest := 0
+		start := 0
+		for i, _ := range line {
+			search := []rune(line[i:len(line)])
+			found := longestFile(search)
+			pos := found + i + 1
+			if found > 0 && pos > longest {
+				longest = pos
+				start = i
+			}
+		}
 
-        if longest > 0 {
-            //fmt.Println(start)
-            //fmt.Println(longest)
-            fmt.Println(strconv.Itoa(i), line[start:longest - 1])
-            for _, v := range argsWithoutProg {
-              n, _ := strconv.Atoi(v)
-              if n == i  {
-                clip.WriteString(line[start:longest-1] + " ")
-                //clipboard.WriteAll(line[start:longest-1])
-              }
-            }
-            //err:=os.Setenv("NW"+ strconv.Itoa(i), line[start:longest -1])
-            if err != nil {
-              fmt.Println("ERROR")
-            }
-        } else {
-            i = i - 1
-            fmt.Print(line)
-        }
-    }
-    clipboard.WriteAll(clip.String())
+		i = i + 1
+
+		if longest > 0 {
+			fmt.Println(strconv.Itoa(i), line[start:longest-1])
+			for _, v := range argsWithoutProg {
+				n, _ := strconv.Atoi(v)
+				if n == i {
+					clip.WriteString(line[start:longest-1] + " ")
+				}
+			}
+		} else {
+			i = i - 1
+			fmt.Print(line)
+		}
+	}
+	clipboard.WriteAll(clip.String())
 }
