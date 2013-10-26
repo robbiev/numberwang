2289e47c84806da4cd7c06683707e600d864f6f0
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2013-10-26 11:52:43 +0100

    refactor

--- nw.go
+++ nw.go
@@ -1,3 +1,4 @@
+// vim: tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab tw=72
 //http://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
 package main
 
@@ -10,27 +11,42 @@ import (
 	"strconv"
 )
 
-func longestFile(line []rune) int {
-	max := 0
+func longestFileEndIndex(line []rune) int {
+	maxIndex := 0
 	for i, _ := range line {
 		slice := line[0:i]
 		file := string(slice)
 		if file != "/" && file != "." && file != "./" && file != ".." && file != "../" {
 			if _, err := os.Stat(file); err == nil {
-				max = i
+				maxIndex = i - 1
 			}
 		}
 	}
-	return max
+	return maxIndex
+}
+
+func longestFileInLine(line string) (firstCharIndex int, lastCharIndex int) {
+	for searchStartIndex, _ := range line {
+		searchSpace := []rune(line[searchStartIndex:len(line)])
+		lastCharIndexInSlice := longestFileEndIndex(searchSpace)
+		lastCharIndexInLine := lastCharIndexInSlice + searchStartIndex
+		if lastCharIndexInSlice > 0 && lastCharIndexInLine > lastCharIndex {
+			lastCharIndex = lastCharIndexInLine
+			firstCharIndex = searchStartIndex
+		}
+	}
+
+	return firstCharIndex, lastCharIndex
 }
 
 func main() {
 	var clip bytes.Buffer
+
 	argsWithoutProg := os.Args[1:]
 
 	reader := bufio.NewReader(os.Stdin)
 
-	lineNumber := 0
+	fileCount := 0
 
 	for {
 		line, err := reader.ReadString('\n')
@@ -40,32 +56,24 @@ func main() {
 			break
 		}
 
-		longest := 0
-		start := 0
-		for i, _ := range line {
-			search := []rune(line[i:len(line)])
-			found := longestFile(search)
-			pos := found + i + 1
-			if found > 0 && pos > longest {
-				longest = pos
-				start = i
-			}
-		}
+		firstCharIndex, lastCharIndex := longestFileInLine(line)
 
-		lineNumber = lineNumber + 1
+		if lastCharIndex > 0 {
+			fileCount++
+			file := line[firstCharIndex : lastCharIndex+1]
 
-		if longest > 0 {
-			file := line[start : longest-1]
-			fmt.Println(strconv.Itoa(lineNumber), file)
+			fmt.Println(strconv.Itoa(fileCount), file)
+
+			// collect any file position arguments to copy to the
+			// clipboard later
 			for _, v := range argsWithoutProg {
 				n, _ := strconv.Atoi(v)
-				if n == lineNumber {
+				if n == fileCount {
 					clip.WriteString(file)
 					clip.WriteString(" ")
 				}
 			}
 		} else {
-			lineNumber = lineNumber - 1
 			fmt.Print(line)
 		}
 	}
