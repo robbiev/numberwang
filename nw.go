b6bd5ec96fade301dad377cb2dcf4750ddf05b13
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2014-04-12 22:05:35 +0100

    tweak flags, better error handling

--- nw.go
+++ nw.go
@@ -87,11 +87,11 @@ func askUser() (requestedNumbers []string, err error) {
 	return strings.Fields(s), nil
 }
 
-type processFunc func(string, string, int, int)
+type PrintFunction func(string, string, int, int)
 
 type Processor interface {
-	processFile(file string)
-	processEnd()
+	processFile(file string) error
+	processEnd() error
 }
 
 type NumbersGiven struct {
@@ -105,75 +105,106 @@ type AskForNumbers struct {
 	files []string
 }
 
-func printProcessor(fileCount *int) processFunc {
+func printShortFormat(fileCount *int) PrintFunction {
 	return func(file string, line string, firstCharIndex int, lastCharIndex int) {
-		fmt.Println(strconv.Itoa(*fileCount), line[:len(line)-1])
+		fmt.Println(strconv.Itoa(*fileCount), file)
 	}
 }
 
-func (ng *NumbersGiven) processEnd() {
-	writeToClipboard(ng.clip)
+func printLongFormat(fileCount *int) PrintFunction {
+	return func(file string, line string, firstCharIndex int, lastCharIndex int) {
+		fmt.Print("{")
+		fmt.Print(strconv.Itoa(*fileCount))
+		fmt.Print("} ")
+		fmt.Print(line[:firstCharIndex])
+		fmt.Print("{")
+		fmt.Print(file)
+		fmt.Print("}")
+		fmt.Print(line[lastCharIndex+1:])
+	}
 }
 
-//	argsWithoutProg := os.Args[1:]
-func (ng *NumbersGiven) processFile(file string) {
+func (ng *NumbersGiven) processEnd() error {
+	writeToClipboard(ng.clip)
+	return nil
+}
 
+func (ng *NumbersGiven) processFile(file string) error {
 	// collect any file position arguments to copy to the
 	// clipboard later
 	for _, v := range ng.numbers {
-		n, _ := strconv.Atoi(v)
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s is not a number\n", v)
+			return err
+		}
 		if n == *ng.fileCount {
 			ng.clip.WriteString(file)
 			ng.clip.WriteString(" ")
 		}
 	}
+	return nil
 }
 
-func (afn *AskForNumbers) processEnd() {
+func (afn *AskForNumbers) processEnd() error {
 	requestedNumbers, err := askUser()
 	if err != nil {
-		fmt.Printf("failed to read input: %s\n", err)
-		return
+		fmt.Fprintf(os.Stderr, "failed to read input: %s\n", err)
+		return err
 	}
 
 	for _, n := range requestedNumbers {
-		i, _ := strconv.Atoi(n)
+		i, err := strconv.Atoi(n)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s is not a number\n", n)
+			return err
+		}
 		afn.clip.WriteString(afn.files[i-1])
 		afn.clip.WriteString(" ")
 	}
 
 	writeToClipboard(afn.clip)
+	return nil
 }
 
-func (afn *AskForNumbers) processFile(file string) {
+func (afn *AskForNumbers) processFile(file string) error {
 	afn.files = append(afn.files, file)
+	return nil
 }
 
 func writeToClipboard(buffer *bytes.Buffer) {
 	clipboardOutput := buffer.String()
 	if clipboardOutput != "" {
 		clipboard.WriteAll(clipboardOutput)
+		fmt.Printf("wrote \"%s\" to clipboard\n", clipboardOutput)
 	}
 }
 
 func main() {
-	ask := flag.Bool("a", false, "ask which file names to copy to the clipboard")
-	flag.Parse()
-
 	var fileCount int
 	var clip bytes.Buffer
 
+	short := flag.Bool("s", false, "short format, only display file names")
+	flag.Parse()
+
+	extraArgs := flag.Args()
+
 	var processor Processor
-	if *ask {
+	if len(extraArgs) == 0 {
 		processor = &AskForNumbers{clip: &clip}
 	} else {
 		processor = &NumbersGiven{
 			clip:      &clip,
 			fileCount: &fileCount,
-			numbers:   os.Args[1:],
+			numbers:   extraArgs,
 		}
 	}
-	printer := printProcessor(&fileCount)
+	var printer PrintFunction
+	if *short {
+		printer = printShortFormat(&fileCount)
+	} else {
+		printer = printLongFormat(&fileCount)
+	}
 
 	reader := bufio.NewReader(os.Stdin)
 
@@ -190,11 +221,17 @@ func main() {
 			file := line[firstCharIndex : lastCharIndex+1]
 
 			printer(file, line, firstCharIndex, lastCharIndex)
-			processor.processFile(file)
-		} else {
+			err := processor.processFile(file)
+			if err != nil {
+				os.Exit(1)
+			}
+		} else if (!*short) {
 			fmt.Print(line)
 		}
 	}
 
-	processor.processEnd()
+	err := processor.processEnd()
+	if err != nil {
+		os.Exit(1)
+	}
 }
